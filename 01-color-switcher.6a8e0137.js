6cf7e01ed6ef4c1a95fbc6220a6c8ca08763fdc1
Mik-Ka <87785123+Mik-Ka@users.noreply.github.com>
2022-08-21 16:36:52 +0000

    Deploying to gh-pages from @ Mik-Ka/goit-js-hw-09@148604b6698247dae1dd16d5c8e73c8f1ac35ed4 🚀

---
+++ 01-color-switcher.6a8e0137.js
@@ -0,0 +1,2 @@
+!function(){var t=null,n={body:document.querySelector("body"),startBtn:document.querySelector("[data-start]"),stopBtn:document.querySelector("[data-stop]")};function e(){n.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}n.startBtn.addEventListener("click",(function(){n.startBtn.disabled=!0,n.stopBtn.disabled=!1,t=setInterval(e,1e3)})),n.stopBtn.addEventListener("click",(function(){clearInterval(t),n.startBtn.disabled=!1,n.stopBtn.disabled=!0}))}();
+//# sourceMappingURL=01-color-switcher.6a8e0137.js.map
